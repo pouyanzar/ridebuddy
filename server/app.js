ee1da08be33bd322079e7046e0159ac1eda188f9
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-25 19:32:49 -0400

    initialize sever and routes

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const profileRoute = require('./routes/profile');
+const loginRoute = require('./routes/login');
+const signupRoute = require('./routes/signup');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/profile', profileRoute);
+app.use('/login', loginRoute);
+app.use('/signup', signupRoute);
+
+module.exports = app;
