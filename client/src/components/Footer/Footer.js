734997fb5d3c2f16a8e41999bcfe85fc97f11273
Saif Ali <saifali7646@gmail.com>
2021-09-25 00:59:31 -0400

    add footer in main page

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import './Footer.css'
+ 
+
+export default function Footer() {
+  
+  return(
+    
+    <div class="footer">
+      <div class="footer-icon">
+       <div>chat</div>
+       <div>mytrip</div>
+       <div>setting</div>
+      </div>
+    </div>  
+  )
+}
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.css';
 import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
  
@@ -10,8 +11,20 @@ export default function Main() {
   return(
     
     <div>
-      <Navbar />
-    </div>    
-
+      <div> <Navbar /></div>
+      
+      <div class="main">
+        <form >
+          <div className= "form">
+            <div className="field"><input  className= "input" type="text" placeholder="Origin"/></div>
+            <div className="field"><input  className= "input" type="text" placeholder="Destination"/></div>
+          </div> 
+          <div className="Button">
+            <Button variant="primary rounded-pill" className="custom-btn" type="submit">Search</Button>
+          </div>
+        </form>
+      </div>
+      <div><Footer /></div>
+    </div>  
   )
 }
