3223a7a211870a16e60c7a2eb53b6c4b4b3cf0c5
Saif Ali <saifali7646@gmail.com>
2021-09-25 18:32:31 -0400

    add icons to Navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className = "nav-logo"> RideBuddy</div>
+      <div className = "nav-logo"><Link to="/"> <img src="https://i.ibb.co/CMS8p17/ridebuddy-logo.jpg" alt="ridebuddy" width="175" height="50"></img></Link></div>
       <div className = "profile-add">
-        <h6>pic</h6>
-        <h6>add</h6>
+        <div className="add-icon"><Link to="/profile"><img className="profile-pic" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></img></Link></div>
+        <div className="profile"><Link to="/add"><i class="fas fa-plus-circle fa-2x fas-white"></i></Link></div>
       </div>
     </div>
   );
