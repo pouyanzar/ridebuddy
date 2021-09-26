0cd5a677a86367b0b96529a33cff55825144a5d8
Saif Ali <saifali7646@gmail.com>
2021-09-25 22:00:44 -0400

    fix search field alignment

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -16,8 +16,8 @@ export default function Main() {
           <div className= "form">
             <div className="field"><input  className= "input" type="text" placeholder="Origin"/></div>
             <div className="field"><input  className= "input" type="text" placeholder="Destination"/></div>
-          </div> 
-          <div className="Button">
+          </div>
+          <div className="search-button">
             <Button variant="primary rounded-pill" className="custom-btn" type="submit">Search</Button>
           </div>
         </form>
