f5f7fc86560df53dc617c18ce7390026667d803f
Saif Ali <saifali7646@gmail.com>
2021-09-27 11:45:06 -0400

    add seeds file for fake data

---
+++ db/seeds/riders.sql
@@ -0,0 +1,10 @@
+-- INSERT PSEUDO USERS DATA INTO OUR DATABASE
+    INSERT INTO riders (user_id, description)
+    VALUES (1, 'Going to see my family, need a company');  
+    INSERT INTO riders (user_id, description)
+    VALUES (1, 'Returning from camping');
+    INSERT INTO riders (user_id, description)
+    VALUES (3, 'Going to attend a ceremony');  
+    INSERT INTO riders (user_id, description)
+    VALUES (6, 'Returning back on sunday');  
+    
