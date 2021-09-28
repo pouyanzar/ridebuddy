b80a8ffa20b3cecdb479af20d2d5c9cef29d1301
Saif Ali <saifali7646@gmail.com>
2021-09-28 16:29:27 -0400

    refactoring cancel booking functionality from myTrips component

--- client/src/components/MyTrips/MyTrips.js
+++ client/src/components/MyTrips/MyTrips.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./MyTrips.css";
 import MyTripsListItem from '../MyTripsListItem/MyTripListItem';
 
 const myTrips = [
   {'id': 1, 'rider': 'Frank', 'seats': 2, 'price': 50,'departure': '12-Dec-2021'},
-  {'id': 2, 'rider': 'Rogers', 'seats': 3,'price': 50, 'departure': '14-Dec-2021'},
+  {'id': 8, 'rider': 'Rogers', 'seats': 3,'price': 50, 'departure': '14-Dec-2021'},
+  {'id': 10, 'rider': 'Joseph', 'seats': 3,'price': 50, 'departure': '14-Dec-2021'}
 ]
 
 export default function MyTrips(props) {
   
-  const cancelBooking = function (name){
-    console.log('Cancelling your Booking with rider', name)
+  const [myTripList, setMyTripList] = useState(myTrips)
+
+  const cancelBooking = function (id){
+  
+    const updatedTrips = myTripList.filter(x => x['id'] !== id) 
+    setMyTripList([...updatedTrips])
+    
   }
   
-  const trip =  myTrips.map((trip) => {
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log(myTripList)
+  },[myTripList]);
+
+  const trip =  myTripList.map((trip) => {
     return(
       <MyTripsListItem
         key={trip.id} 
@@ -22,7 +33,7 @@ export default function MyTrips(props) {
         seats= {trip.seats}
         price={trip.price}
         departure = {trip.departure} 
-        cancelBooking = {(event)=>cancelBooking(trip.rider)}
+        cancelBooking = {(event)=>cancelBooking(trip.id)}
       />)
   })
 
