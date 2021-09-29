20e41f3b6e7efe3e24612625d33bf09c64efbc58
Saif Ali <saifali7646@gmail.com>
2021-09-28 21:13:48 -0400

    remove db file from the root

--- db/schema/passengers.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS passengers CASCADE;
-
--- Create passengers table
-CREATE TABLE passengers (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  trip_id INTEGER REFERENCES trips(id) ON DELETE CASCADE
-);
