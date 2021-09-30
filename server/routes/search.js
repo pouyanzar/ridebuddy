5fe0a50fb58a07eba7ce4221a4ac31537b9a5542
Saif Ali <saifali7646@gmail.com>
2021-09-29 23:26:28 -0400

    update SQL query to update booking seats

--- server/routes/search.js
+++ server/routes/search.js
@@ -21,8 +21,30 @@ module.exports = (db) => {
     
     db.query(`SELECT trips.id, users.name as rider, avatar, price, available_seats as seats, available_luggages as luggage, pic, departure FROM trips 
       JOIN users ON users.id = user_id 
-      WHERE origin = $1 AND destination = $2;
+      WHERE origin = $1 AND destination = $2
+      ORDER BY departure ASC;
       `, [origin, destination])
+      .then(({ rows: trips }) => {
+        res.json({ rows: trips })
+      })
+      .catch(err => {
+        res.send().status(500)
+        .json({error: err.message});
+      });
+  });
+
+  router.post("/:id", (req, res) => {
+    
+    const trip_id = req.body.id;
+    const seats = req.body.seats;
+
+    console.log('trp_id', trip_id);
+    console.log('seats', seats);
+
+    db.query(`UPDATE trips
+      SET available_seats = $1
+      WHERE trips.id = $2;
+      `, [seats, trip_id])
       .then(({ rows: trips }) => {
         res.json({ rows: trips })
       })
